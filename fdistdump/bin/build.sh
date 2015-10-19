36982cdcfd57b2ca23ee41e448e70535edc8126d
bodik <bodik@cesnet.cz>
2015-10-20 01:13:55 +0200

    postinst and postrm scripts for libnf (ldconfig) some other tuning

--- fdistdump/bin/build.sh
+++ fdistdump/bin/build.sh
@@ -2,11 +2,13 @@
 
 set -e
 
-BUILD_AREA=/opt/fdistdump/build_area
 git config --global user.name "bodik"
 git config --global user.email "bodik@cesnet.cz"
 #git commit --amend --author="Radoslav Bodo <bodik@cesnet.cz>"
 
+BUILD_AREA=/opt/fdistdump/build_area
+rm -r ${BUILD_AREA} || true
+dpkg --purge libnf fdistdump 2>/dev/null
 mkdir -p $BUILD_AREA
 
 cd $BUILD_AREA || exit 1
@@ -21,6 +23,7 @@ make DESTDIR="${BUILD_AREA}/libnf-install" install
 cd ..
 for target in deb rpm; do 
 	fpm -s dir -t $target -C "${BUILD_AREA}/libnf-install" --name libnf --version ${VER} --iteration 1  \
+		--after-install /puppet/fdistdump/files/libnf.postinst --after-remove /puppet/fdistdump/files/libnf.postrm \
 		--description "libnf package from libnf.net/packages" --maintainer "bodik@cesnet.cz" --vendor "" --url "http://libnf.net"
 done
 dpkg -i libnf_${VER}-1_$(dpkg --print-architecture).deb
--- fdistdump/bin/get_test_data.sh
+++ fdistdump/bin/get_test_data.sh
@@ -4,6 +4,6 @@ cd /opt/fdistdump || exit 1
 if [ ! -d data ]; then
 	mkdir data
 	cd data || exit 1
-	wget --no-parent -m --no-directories http://esb.metacentrum.cz/puppet-fdistdump.git-testdata/
+	wget --no-parent -m --no-directories --no-verbose http://esb.metacentrum.cz/puppet-fdistdump.git-testdata/
 fi
 
