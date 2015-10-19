06fa4f3b2d0f2d382a7a4b7ba13d0f892d3ea0ca
bodik <bodik@cesnet.cz>
2015-10-19 22:52:22 +0200

    basic tests

---
+++ fdistdump/tests/fdistdump-singlenode.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+. /puppet/metalib/bin/lib.sh
+
+BASE=/opt/fdistdump
+cd ${BASE} || exit 1
+
+for flow_file in $(find data -name "nfcapd*"); do
+	CMD="mpirun -np 2 bin/fdistdump -r ${flow_file} -s dstport"
+	$CMD
+	if [ $? -ne 0 ]; then
+		rreturn 1 "$0 $CMD"
+	fi
+done
+
+rreturn 0 "$0 ok"
+
