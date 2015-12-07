aff2bbef13f0bda349acf06c0954f95bff3fee39
root <root@took64.ics.muni.cz>
2015-12-07 21:49:10 +0100

    first node in mpirun is master, it must be doubled in case there are data to process on that node

--- fdistdump/tests/fdistdump-cluster.sh
+++ fdistdump/tests/fdistdump-cluster.sh
@@ -6,6 +6,7 @@ BASE=/opt/fdistdump
 cd ${BASE} || exit 1
 
 NODES=$(curl -s http://$(facter fqdn):39200/_cat/nodes | awk '{printf $1","}')
-mpirun --host $NODES fdistdump -r "${BASE}/${flow_file}/data" -s dstport
+MASTER=$(echo $NODES | awk -F',' '{print $1}')
+mpirun --host $MASTER,$NODES fdistdump -r "${BASE}/${flow_file}/data" -s dstport
 
 rreturn $? "$0"
