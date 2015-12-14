3546a76ee323b52c9d8b3c60438bbb8a75a8dfca
bodik <bodik@cesnet.cz>
2015-12-14 10:20:05 +0100

    buildstub package + ipfixcol single final package

--- securitycloud/bin/standalone.sh
+++ securitycloud/bin/standalone.sh
@@ -4,7 +4,7 @@ set -x
 echo 'deb http://esb.metacentrum.cz/puppet-securitycloud-packages ./' > /etc/apt/sources.list.d/securitycloud.list
 echo 'APT::Get::AllowUnauthenticated yes;' > /etc/apt/apt.conf.d/99auth
 apt-get update
-apt-get install -y fdistdump ipfixcol-base ipfixcol-plugins
+apt-get install -y fdistdump ipfixcol
 
-dpkg -l fdistdump libnf ipfixcol-base ipfixcol-plugins
+dpkg -l fdistdump libnf ipfixcol
 
