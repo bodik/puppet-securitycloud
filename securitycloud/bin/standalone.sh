46f37932401f35fd989e0d1dd7827a1d7a7092ad
bodik <bodik@cesnet.cz>
2015-12-18 14:16:35 +0100

    multi distro standalone

--- securitycloud/bin/standalone.sh
+++ securitycloud/bin/standalone.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 set -x
 
-wget -qO - http://esb.metacentrum.cz/puppet-securitycloud-packages/securitycloud.asc | apt-key add -
-echo 'deb http://esb.metacentrum.cz/puppet-securitycloud-packages/debian jessie main' > /etc/apt/sources.list.d/securitycloud.list
-apt-get update
-apt-get -y install fdistdump ipfixcol
-dpkg -l fdistdump libnf ipfixcol
+case "$(facter osfamily)" in
+    "Debian")
+	wget -qO - http://esb.metacentrum.cz/puppet-securitycloud-packages/securitycloud.asc | apt-key add -
+	echo 'deb http://esb.metacentrum.cz/puppet-securitycloud-packages/debian jessie main' > /etc/apt/sources.list.d/securitycloud.list
+	apt-get update
+	apt-get -y install fdistdump ipfixcol
+	dpkg -l fdistdump libnf ipfixcol
+    ;;
+    "RedHat")
+	echo "not implemented"
+	exit 1
+    ;;
+esac
+
 
