ddb8321e7a55b27ca816cf65a209309d4f8e5e51
root <root@took26.ics.muni.cz>
2016-08-12 16:51:09 +0200

    check on running processes

--- securitycloud/tests/securitycloud-ipfixcol.sh
+++ securitycloud/tests/securitycloud-ipfixcol.sh
@@ -34,4 +34,14 @@ if [ ! -f /data/conf/ipfixcol/startup-subcollector.xml ]; then
 fi
 
 
+# this is not nice, since there might be "vim ipfixcol.xml" running
+# but the processes are named ipfixcol:udp-cpg and ipfixcol:TCP
+# and simple check like this one does not have a clue about pacemaker logic ...
+check_procs -a ipfixcol -c 1:2
+if [ $? -ne 0 ]; then
+	rreturn 1 "$0 ipfixcol check_procs"
+fi
+
+
+
 rreturn 0 "$0"
