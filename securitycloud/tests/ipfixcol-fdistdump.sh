4376417a03e083a4205310112d35cc3d38b76c7c
root <root@took26.ics.muni.cz>
2016-08-15 09:54:16 +0200

    fdistdump cloud impl

--- securitycloud/tests/ipfixcol-fdistdump.sh
+++ securitycloud/tests/ipfixcol-fdistdump.sh
@@ -2,11 +2,6 @@
 
 . /puppet/metalib/bin/lib.sh
 
-BASE=/var/lib/ipfixcol/lnfstore/
-cd ${BASE} || exit 1
-
-DATA=$(securitycloud.init list | grep " data " | awk '{printf $2","}')
-MASTER=$(securitycloud.init list | grep " master " | awk '{print $2}')
-mpirun --host $MASTER,$DATA fdistdump -s dstport "${BASE}"
+fdistdump-ha -f "-s srcport" $(date +%Y)
 
 rreturn $? "$0"
