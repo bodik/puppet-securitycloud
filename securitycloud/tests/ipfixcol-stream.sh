4d3f763b1ae1ee4670208adbd6302a594df91e22
root <root@took26.ics.muni.cz>
2016-08-16 18:56:31 +0200

    stream to virtualip

--- securitycloud/tests/ipfixcol-stream.sh
+++ securitycloud/tests/ipfixcol-stream.sh
@@ -1,4 +1,4 @@
 for all in $(find /scratch/ipfixcol/data -type f); do
 	echo "INFO: streaming $all $(ls -lh $all)"
-	ipfixsend -i $all -d "$(securitycloud.init proxy)" -n 1 -t UDP -p 4739 -s 5M
+	ipfixsend -i $all -d $(securitycloud.init virtualip) -R 1.0 -n 1
 done
