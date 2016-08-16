0af5285edca926d6a1be5ac474ba4e11bdc8e4fc
root <root@took26.ics.muni.cz>
2016-08-16 21:47:21 +0200

    new complex test

--- securitycloud/tests/ipfixcol-complex02.sh
+++ securitycloud/tests/ipfixcol-complex02.sh
@@ -10,15 +10,19 @@ echo "INFO: cluster list"
 securitycloud.init list
 pcs status
 
-echo "INFO: stream example01"
-sh -x /puppet/securitycloud/tests/ipfixcol-stream-example01.sh
+echo "INFO: fetch ipfixcol testdata"
+sh /puppet/securitycloud/bin/testdata-ipfixcol-get.sh
+
+
+echo "INFO: stream testdata"
+sh -x /puppet/securitycloud/tests/ipfixcol-stream.sh
 echo "INFO: restarting ipfixcol services to flush streamed data"
 securitycloud.init istop
 securitycloud.init istart
 echo "INFO: querying data"
-FLOWS=3979
-PACKETS=56695
-BYTES=49001404
+FLOWS=3192195
+PACKETS=90920552
+BYTES=97051395875
 sh /puppet/securitycloud/tests/ipfixcol-fdistdump.sh | grep "$FLOWS flows, $PACKETS packets, $BYTES bytes"
 if [ $? -ne 0 ]; then
 	rreturn 1 "$0 results not correct"
@@ -38,9 +42,9 @@ securitycloud.init list
 pcs status
 
 REPS=$(/bin/bash -c 'echo $(( $RANDOM % 10 ))')
-echo "INFO: repeating stream example01 $REPS times"
+echo "INFO: repeating stream tesdata $REPS times"
 for i in $(seq 1 $REPS); do
-	sh /puppet/securitycloud/tests/ipfixcol-stream-example01.sh
+	sh /puppet/securitycloud/tests/ipfixcol-stream.sh
 done
 echo "INFO: restarting ipfixcol services to flush streamed data"
 securitycloud.init istop
