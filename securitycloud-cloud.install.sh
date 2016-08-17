71df54e827357f6f63076767453cab05f521d1c0
root <root@took38.ics.muni.cz>
2016-08-17 18:22:13 +0000

    abort on error

--- securitycloud-cloud.install.sh
+++ securitycloud-cloud.install.sh
@@ -1,8 +1,11 @@
+#!/bin/sh
+set -e
+
 pa.sh -e "include securitycloud::cloud"
 
-cd /usr/local/SecurityCloud/install/ || exit 1
+cd /usr/local/SecurityCloud/install/
 ./install.sh check
-./install.sh glusterfs
+./install.sh glusterfs 
 ./install.sh ipfixcol
 ./install.sh fdistdump
 ./install.sh stack
